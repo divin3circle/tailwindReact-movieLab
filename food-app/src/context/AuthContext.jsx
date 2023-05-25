f85a3ca0c8cc91ea64377dd863f2620d6b6aca6e
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-25 12:32:22 +0300

    firebase-auth

--- food-app/src/context/AuthContext.jsx
+++ food-app/src/context/AuthContext.jsx
@@ -30,7 +30,7 @@ export function AuthContextProvider({ children }) {
     return () => {
       unsubscribe();
     };
-  }, []);
+  });
 
   return (
     <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
--- food-app/src/firebase.js
+++ food-app/src/firebase.js
@@ -7,13 +7,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_API_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_API_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_API_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_API_FIREBASE_MESSAGING_SENDER,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: "G-KXMNKSC0R4",
+  apiKey: "AIzaSyCgm-4wZLgFq38O01bh2cWUCU7kFHgYQBQ",
+  authDomain: "movielab-de120.firebaseapp.com",
+  projectId: "movielab-de120",
+  storageBucket: "movielab-de120.appspot.com",
+  messagingSenderId: "687659644065",
+  appId: "1:687659644065:web:0d4027a3c7b04d3037ace0",
 };
 
 // Initialize Firebase
